ac4b4908ba1540c2bdc62db4faba2b521895c514
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 13:33:49 +0000

    Sun Jun 17 13:33:49 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -17,9 +17,14 @@ def checkout(skus):
                 found_A = count
         elif(letter == 'B'):
             if(found_B == 0):
-
-                total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30)
+                count_E = skus.count(letter)
+                if (count_E >= 2) :
+                    final_count = count - (int(count_E / 2))
+                    if final_count < 0:
+                        count = 0
+                total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30) + count_E * 40
                 found_B = count
+                found_E = count_E
         elif(letter == 'C'):
             if(found_C == 0) :
                 total_amount = total_amount + count * 20
@@ -31,10 +36,15 @@ def checkout(skus):
         elif(letter == 'E'):
             if (found_E == 0):
                 if (count >= 2):
-                    total_amount = total_amount + count * 40 - (count % 2) * 30
+                    count_B = skus.count('B')
+                    final_count = count_B - int(count/2)
+                    if (final_count < 0):
+                        final_count = 0
+                        total_amount = total_amount + count * 40 + (int((final_count / 2)) * 45) + ((final_count % 2) * 30)
                 else:
                     total_amount = total_amount + count * 40
                 found_E = count
+                found_B = count_B
         else:
             return -1
     return total_amount
