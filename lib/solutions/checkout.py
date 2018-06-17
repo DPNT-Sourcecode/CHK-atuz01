d651c84ad9e3b2c569c71a0f956e6afae059525b
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 12:16:56 +0000

    Sun Jun 17 12:16:56 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,5 +3,18 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-
+    total_amount = 0;
+    for letter in skus:
+        count = skus.count(letter)
+        if(letter == 'A'):
+            total_amount = (count // 3) * 130 + (count % 3) * 50
+        elif(letter == 'B'):
+            total_amount = (count//2) * 45 + (count % 2) * 30
+        elif(letter == 'C') :
+            total_amount = count * 20
+        elif(letter == 'D'):
+            total_amount = count * 15
+        else:
+            return -1
+    return total_amount
     raise NotImplementedError()
