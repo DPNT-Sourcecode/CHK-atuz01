71a75a526e30d6654c23001f572dde10df50f195
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 12:36:04 +0000

    Sun Jun 17 12:36:04 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,25 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    found_A = false
-    found_B = false
-    found_C = false
-    found_D = false
+    found_A = 0
+    found_B = 0
+    found_C = 0
+    found_D = 0
     total_amount = 0
     for letter in skus:
         count = skus.count(letter)
-        if(letter == 'A' and found_A == false):
+        if(letter == 'A' and found_A == 0):
              total_amount = total_amount + (int((count / 3)) * 130) + ((count % 3) * 50)
-             found_A = true
-        elif(letter == 'B' and found_B == false):
+             found_A = 1
+        elif(letter == 'B' and found_B == 0):
             total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30)
-            found_B = true
-        elif(letter == 'C' and found_C == false) :
+            found_B = 1
+        elif(letter == 'C' and found_C == 0) :
             total_amount = total_amount + count * 20
-            found_C = true
-        elif(letter == 'D'and found_D == false):
+            found_C = 1
+        elif(letter == 'D'and found_D == 0):
             total_amount =  total_amount + count * 15
-            found_D = true
+            found_D = 1
         else:
             return -1
     return total_amount
