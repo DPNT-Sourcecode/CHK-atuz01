f8784e2411fb6757be0e1719baa9956539bb4001
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 15:14:49 +0000

    Sun Jun 17 15:14:49 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -6,15 +6,15 @@ def checkout(skus):
     prices = {'A' : (50 ,0 ), 'B' : (30, 0), 'C' : (20, 0), 'D' : (15,0), 'E' : (40,0), 'F' : (10,0) ,
              'G' : (20,0) ,'H' : (10, 0), 'I' : (35, 0) ,'J' : (60, 0) , 'K' : (80, 0) , 'L' : (90,0),
               'M': (15,0) , 'N' : (40, 0) , 'O' : (10, 0), 'P' :(50, 0) , 'Q' : (30, 0) , 'R' :(50,0) ,
-              'S': (30,0), 'T' :(20,0), 'U' : (40,0) , 'V' : (50, 0), 'W' : (20,0), 'X' : (90, 0), 'Y': (10,0)
+              'S': (30,0), 'T' :(20,0), 'U' : (40,0) , 'V' : (50, 0), 'W' : (20,0), 'X' : (90, 0), 'Y': (10,0),
               'Z': (50,0)}
     total_amount = 0
     for letter in skus:
         count = skus.count(letter)
         if(letter == 'A'):
             if(prices.get(letter)[1] == 0):
-                total_amount = total_amount + (int((count / 5)) * 200) + (int((count % 5)/3) * 130) +
-                (int((count % 5 ) % 3)) * prices.get(letter)[0]
+                total_amount = total_amount + (int((count / 5)) * 200) + (int((count % 5)/3) * 130) +\
+                               (int((count % 5 ) % 3)) * prices.get(letter)[0]
                 prices.get(letter)[1] = count
         elif(letter == 'B'):
             if(prices.get(letter)[1] == 0):
@@ -56,8 +56,8 @@ def checkout(skus):
                 elif ( count < 2) :
                     total_amount = total_amount + count * prices.get(letter)[0]
                 else:
-                    total_amount = total_amount + count * prices.get(letter)[0] - ((count % 2) * prices.get(letter)[0])
-                 prices.get(letter)[1] = count
+                    total_amount = total_amount + count * prices.get(letter)[0] - (count % 2) * prices.get(letter)[0]
+                prices.get(letter)[1] = count
         elif (letter == 'H'):
             if(prices.get(letter)[1] == 0):
               total_amount = total_amount + int(count/10) * 80 + + (int((count % 10)/5) * 45) + \
@@ -72,9 +72,19 @@ def checkout(skus):
               total_amount = total_amount + int(count/5) * 200 + (count % 5) * prices.get(letter)[0]
             prices.get(letter)[1] = count
         elif letter == 'V':
-            if (prices.get(letter)[1] == 0):
-                total_amount = total_amount + int(count / 3) * 80 + (count % 3) * prices.get(letter)[0]
+            if(prices.get(letter)[1] == 0):
+              total_amount = total_amount + int(count/3) * 130 + (int((count % 3)/2) * 90) + \
+                             (int((count % 3 ) % 2)) * prices.get(letter)[0]
             prices.get(letter)[1] = count
+        elif(letter == 'U'):
+            if (prices.get(letter)[1] == 0):
+                if( count % 3 == 0 and count >= 3) :
+                    total_amount = total_amount + count * prices.get(letter)[0] - (int(count / 3) - 1 ) * prices.get(letter)[0]
+                elif ( count < 3) :
+                    total_amount = total_amount + count * prices.get(letter)[0]
+                else:
+                    total_amount = total_amount + count * prices.get(letter)[0] - (count % 3) * prices.get(letter)[0]
+                prices.get(letter)[1] = count
 
         else:
             return -1
