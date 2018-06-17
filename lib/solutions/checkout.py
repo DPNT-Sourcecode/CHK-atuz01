8009f32bf298a04ac59ebe17a2b1b5684a4bad3d
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 12:34:42 +0000

    Sun Jun 17 12:34:42 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,17 +3,25 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    found_A = false
+    found_B = false
+    found_C = false
+    found_D = false
     total_amount = 0
     for letter in skus:
         count = skus.count(letter)
-        if(letter == 'A'):
+        if(letter == 'A' and found_A == false):
              total_amount = total_amount + (int((count / 3)) * 130) + ((count % 3) * 50)
-        elif(letter == 'B'):
+             found_A = true
+        elif(letter == 'B' and found_B == false):
             total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30)
-        elif(letter == 'C') :
+            found_B = true
+        elif(letter == 'C' and found_C == false) :
             total_amount = total_amount + count * 20
-        elif(letter == 'D'):
+            found_C = true
+        elif(letter == 'D'and found_D == false):
             total_amount =  total_amount + count * 15
+            found_D = true
         else:
             return -1
     return total_amount
