a3e55e8c0ca2792fac5df747eaa9df65365236bf
sbx_user1059 <sbx_user1059@ip-10-11-202-52.eu-west-2.compute.internal>
2018-06-17 12:37:53 +0000

    Sun Jun 17 12:37:53 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -10,18 +10,22 @@ def checkout(skus):
     total_amount = 0
     for letter in skus:
         count = skus.count(letter)
-        if(letter == 'A' and found_A == 0):
-             total_amount = total_amount + (int((count / 3)) * 130) + ((count % 3) * 50)
-             found_A = 1
-        elif(letter == 'B' and found_B == 0):
-            total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30)
-            found_B = 1
-        elif(letter == 'C' and found_C == 0) :
-            total_amount = total_amount + count * 20
-            found_C = 1
-        elif(letter == 'D'and found_D == 0):
-            total_amount =  total_amount + count * 15
-            found_D = 1
+        if(letter == 'A'):
+            if(found_A == 0):
+                total_amount = total_amount + (int((count / 3)) * 130) + ((count % 3) * 50)
+                found_A = 1
+        elif(letter == 'B'):
+            if(found_B == 0):
+                total_amount = total_amount + (int((count / 2)) * 45) + ((count % 2) * 30)
+                found_B = 1
+        elif(letter == 'C'):
+            if(found_C == 0) :
+                total_amount = total_amount + count * 20
+                found_C = 1
+        elif(letter == 'D'):
+            if(found_D == 0):
+                total_amount =  total_amount + count * 15
+                found_D = 1
         else:
             return -1
     return total_amount
